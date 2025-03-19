b8ff1416b0d8a0b443969d83b0e1479f85b65e3f
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-03-19 20:16:51 +0530

    pp

--- my_app/config/dev.exs
+++ my_app/config/dev.exs
@@ -3,10 +3,10 @@ import Config
 # Configure your database
 config :my_app, MyApp.Repo,
   username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  password: "admin",
   database: "my_app_dev",
-  stacktrace: true,
+  hostname: "localhost",
+  port: 5432,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
